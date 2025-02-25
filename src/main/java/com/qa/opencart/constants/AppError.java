511a9cdcf964f0db81f1946a9d8ab83f342aabc6
shravani m <shravani.madhire@gmail.com>
2025-02-25 20:06:03 +0530

    added footer link test cases

--- src/main/java/com/qa/opencart/constants/AppError.java
+++ src/main/java/com/qa/opencart/constants/AppError.java
@@ -5,6 +5,8 @@ public class AppError {
 	public final static String TITLE_NOT_FOUND_ERROR = "==title is not matched==";
 	public final static String URL_NOT_FOUND_ERROR = "==url is not matched==";
 	public final static String ELEMENT_NOT_FOUND_ERROR = "==element not found==";
+	public final static String LOGO_NOT_FOUND_ERROR = "==Logo not found==";
+
 	
 	
 	public final static String PRODUCT_NOT_FOUND_ERROR = "==product not found==";
--- src/main/java/com/qa/opencart/pages/CommonsPage.java
+++ src/main/java/com/qa/opencart/pages/CommonsPage.java
@@ -1,7 +1,11 @@
 package com.qa.opencart.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.util.ElementUtil;
@@ -19,8 +23,28 @@ private ElementUtil eleUtil;
 	}
 	
 	private By logo = By.className("img-responsive");
+	private By footer = By.xpath("//footer//a");
 	private By myAccountDropdown = By.xpath("//a[@title=\"My Account\"]");
 	private By myAccountOption = By.xpath("//li[@class=\"dropdown open\"]//a[text()='My Account']");
+	
+	public boolean isLogoDisplayed() {
+		return eleUtil.doIsElementDisplayed(logo);
+	}
+	
+	public List<String> getFooterList() {
+		List<WebElement> footerList = eleUtil.waitForElementsPresence(footer, AppConstants.DEFAULT_TIME_OUT);
+		System.out.println("Total number of footers: " +footerList.size());
+		List<String> footers = new ArrayList<String>();
+		for(WebElement e : footerList){
+			String text = e.getText();
+			footers.add(text);
+		}
+		return footers;
+	}
+	
+	public boolean checkFooterLink(String footerLink) {
+		return getFooterList().contains(footerLink);	
+	}
 	
 	
 	public HomePage selectAccountOption(){
